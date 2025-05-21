fc2b58ca2ab36b99839083497a5f65d3c8450df0
Remi Bernotavicius <bobbobbio@gmail.com>
2025-05-21 22:59:10 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,15 +11,40 @@ sudo apt install -y \
 
 # install fish
 pushd fish-shell
+
 mkdir build
 pushd build
+
 cmake ..
 cmake --build .
 sudo cmake --install .
 
+popd # build
+popd # fish-shell
+
 # install tmux
+pushd tmux
+
+sh autogen.sh
+./configure
+make -j12
+sudo make install
+
+popd # tmux
+
+cp .tmux.conf ~/
 
 # install vim
+pushd vim
+./configure --with-features=huge --enable-pythoninterp
+make -j12
+sudo make install
+
+popd # vim
+
+cp .vimrc ~/
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
 
 # other utilities
 cargo install cargo-binstall
